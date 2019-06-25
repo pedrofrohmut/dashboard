17c1a4814348539e6e77b46b3ba842d5d7721a01
Pedro Frohmut <pedrofrohmut@gmail.com>
2019-06-25 18:42:41 -0300

    styled sidebar and navbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <app-navbar></app-navbar>
-    <main class="row" id="main-content">
-      <app-sidebar class="col-sm-3"></app-sidebar>
-      <div class="col-sm-9" id="dashboard">
-        <p>Content goes here.</p>
-      </div>
-    </main>
-  `,
-  styles: []
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'client';
+  title = "client"
 }
