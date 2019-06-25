198530f77ac15f405069a785a115bf28754593e5
Pedro Frohmut <pedrofrohmut@gmail.com>
2019-06-25 17:59:40 -0300

    Recreate Angular Client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from "@angular/core"
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  template: `
+    <app-navbar></app-navbar>
+    <main class="row" id="main-content">
+      <app-sidebar class="col-sm-3"></app-sidebar>
+      <div class="col-sm-9" id="dashboard">
+        <p>Content goes here.</p>
+      </div>
+    </main>
+  `,
+  styles: []
 })
 export class AppComponent {
-  title = "client"
+  title = 'client';
 }
