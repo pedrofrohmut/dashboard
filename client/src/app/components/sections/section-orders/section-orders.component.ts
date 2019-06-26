aed99daad1fd9a14cf49ad6ed73daf3f2be8c8de
Pedro Frohmut <pedrofrohmut@gmail.com>
2019-06-26 12:01:27 -0300

    Orders Table with Mock Data

--- client/src/app/components/sections/section-orders/section-orders.component.ts
+++ client/src/app/components/sections/section-orders/section-orders.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
+import Order from "src/app/shared/models/Order"
+import { MOCK_ORDERS } from "src/app/shared/orders.mockdata"
 
 @Component({
-  selector: 'app-section-orders',
-  templateUrl: './section-orders.component.html',
-  styleUrls: ['./section-orders.component.css']
+  selector: "app-section-orders",
+  templateUrl: "./section-orders.component.html",
+  styleUrls: ["./section-orders.component.css"],
 })
 export class SectionOrdersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  public orders: Order[] = MOCK_ORDERS
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
