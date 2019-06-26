5b34121e43f243f854a0ee974a5bf5c45a54b928
Pedro Frohmut <pedrofrohmut@gmail.com>
2019-06-26 08:50:08 -0300

    Bar Chart and Prettier

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser"
 import { NgModule } from "@angular/core"
 
+import { ChartsModule } from "ng2-charts"
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { NavbarComponent } from "./components/navbar/navbar.component"
@@ -8,6 +9,9 @@ import { SidebarComponent } from "./components/sidebar/sidebar.component"
 import { SectionSalesComponent } from "./components/sections/section-sales/section-sales.component"
 import { SectionOrdersComponent } from "./components/sections/section-orders/section-orders.component"
 import { SectionHealthComponent } from "./components/sections/section-health/section-health.component"
+import { BarChartComponent } from "./components/charts/bar-chart/bar-chart.component"
+import { LineChartComponent } from "./components/charts/line-chart/line-chart.component"
+import { PieChartComponent } from "./components/charts/pie-chart/pie-chart.component"
 
 @NgModule({
   declarations: [
@@ -16,10 +20,13 @@ import { SectionHealthComponent } from "./components/sections/section-health/sec
     SidebarComponent,
     SectionSalesComponent,
     SectionOrdersComponent,
-    SectionHealthComponent
+    SectionHealthComponent,
+    BarChartComponent,
+    LineChartComponent,
+    PieChartComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ChartsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
