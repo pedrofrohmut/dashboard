ebb33ef29b496ebe6e42f6157832302fc788b129
Pedro Frohmut <pedrofrohmut@gmail.com>
2019-07-02 15:25:32 -0300

    Finished pagination

--- client/src/app/components/pagination/pagination.component.ts
+++ client/src/app/components/pagination/pagination.component.ts
@@ -3,24 +3,36 @@ import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core"
 @Component({
   selector: "app-pagination",
   template: `
-    <nav aria-label="pagination">
-      <ul class="pagination text-dark">
-        <li class="page-item">
-          <a class="page-link" (click)="onPrevius()">Previous</a>
-        </li>
-        <li class="page-item current-page">
-          <a class="page-link">{{ currentPage }} of {{ pagesCount }}</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" (click)="onNext()">Next</a>
-        </li>
-      </ul>
-    </nav>
+    <div class="flex-container">
+      <span class="text-white float-left mr-4 py-2">
+        Page {{ currentPage }} of {{ pagesCount }}
+      </span>
+      <nav aria-label="pagination">
+        <ul class="pagination">
+          <li class="page-item">
+            <button class="page-link text-dark" (click)="onPrevius()">
+              Previous
+            </button>
+          </li>
+
+          <li class="page-item" *ngFor="let pageNumber of pagesChunk">
+            <button class="page-link text-dark" (click)="onToPage(pageNumber)">
+              {{ pageNumber }}
+            </button>
+          </li>
+
+          <li class="page-item">
+            <button class="page-link text-dark" (click)="onNext()">Next</button>
+          </li>
+        </ul>
+      </nav>
+    </div>
   `,
   styles: [
     `
-      ul {
-        cursor: pointer;
+      .flex-container {
+        display: flex;
+        justify-content: center;
       }
     `,
   ],
@@ -30,6 +42,7 @@ export class PaginationComponent implements OnInit {
   @Input() pageSize: number
   @Input() pagesCount: number
   @Input() isLoading: boolean
+  @Input() pagesChunk: number[]
 
   @Output() onEmitPrevius = new EventEmitter()
   @Output() onEmitNext = new EventEmitter()
@@ -45,6 +58,7 @@ export class PaginationComponent implements OnInit {
 
   onNext() {
     this.onEmitNext.emit()
+    console.log(this.pagesChunk)
   }
 
   onToPage(pageNumber: number) {
--- client/src/app/components/sections/section-orders/section-orders.component.ts
+++ client/src/app/components/sections/section-orders/section-orders.component.ts
@@ -1,7 +1,33 @@
 import { Component, OnInit } from "@angular/core"
 import Order from "src/app/shared/models/Order"
-import { MOCK_ORDERS } from "src/app/shared/orders.mockdata"
 import { SalesDataService } from "src/app/services/sales-data.service"
+
+const getPagesChunkFor = (midVal: number, size: number): number[] => {
+  const result = []
+  const slice = Math.floor(size / 2)
+  const start = midVal - slice
+  const end = midVal + slice
+  for (let i = start; i <= end; i++) {
+    result.push(i)
+  }
+  return result
+}
+
+const getPagesChunk = (currentPage: number, pagesCount: number) => {
+  // always odd number for chunkSize
+  const chunkSize = 11
+  const chunkSlice = Math.floor(chunkSize / 2)
+
+  if (currentPage - chunkSlice <= 0) {
+    return getPagesChunkFor(chunkSlice + 1, chunkSize)
+  }
+
+  if (currentPage + 5 > pagesCount) {
+    return getPagesChunkFor(pagesCount - chunkSize - 1, chunkSize)
+  }
+
+  return getPagesChunkFor(currentPage, chunkSize)
+}
 
 @Component({
   selector: "app-section-orders",
@@ -14,6 +40,7 @@ export class SectionOrdersComponent implements OnInit {
   currentPage: number = 1
   pageSize: number = 10
   isLoading: boolean = true
+  pagesChunk: number[] = getPagesChunk(this.currentPage, this.pagesCount)
 
   constructor(private salesDataService: SalesDataService) {}
 
@@ -23,7 +50,6 @@ export class SectionOrdersComponent implements OnInit {
 
   getOrdersPage(pageIndex: number, pageSize: number) {
     this.salesDataService.getOrders(pageIndex, pageSize).subscribe((res) => {
-      console.log(res)
       this.orders = res.data
       this.pagesCount = res.pagesCount
       this.isLoading = false
@@ -34,6 +60,7 @@ export class SectionOrdersComponent implements OnInit {
     if (this.currentPage > 1) {
       this.currentPage--
       this.getOrdersPage(this.currentPage, this.pageSize)
+      this.pagesChunk = getPagesChunk(this.currentPage, this.pagesCount)
     }
   }
 
@@ -41,6 +68,7 @@ export class SectionOrdersComponent implements OnInit {
     if (this.currentPage < this.pagesCount) {
       this.currentPage++
       this.getOrdersPage(this.currentPage, this.pageSize)
+      this.pagesChunk = getPagesChunk(this.currentPage, this.pagesCount)
     }
   }
 
